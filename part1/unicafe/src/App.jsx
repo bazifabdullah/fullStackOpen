4a148cf4e63871250320a4317688d9b25c31a264
Bazif Abdullah <bazifabdullah@gmail.com>
2025-04-10 14:03:49 +0500

    Complete exercise 6 of part 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,8 +1,48 @@
-function App() {
+import { useState } from 'react'
+
+const Header = (props) => (
+  <h1>{props.text}</h1>
+)
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
+
+const StatisticsValue = (props) => (
+  <p>
+    {props.text} = {props.value}
+  </p>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      Hello
+      <Header text="Give Feedback" />
+
+      <Button onClick={handleGoodClick} text='Good' />
+      <Button onClick={handleNeutralClick} text='Neutral' />
+      <Button onClick={handleBadClick} text='Bad' />
+      
+      <Header text='Statistics' />
+      <StatisticsValue text='Good' value={good} />
+      <StatisticsValue text='Neutral' value={neutral} />
+      <StatisticsValue text='Bad' value={bad} />
     </div>
   )
 }
