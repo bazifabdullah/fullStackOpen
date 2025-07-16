8f0e9065a065691a7cbc4a7754a985dbb4494e8c
Bazif Abdullah <bazifabdullah@gmail.com>
2025-07-17 01:22:11 +0500

    Complete exercise 19 of part2

--- part2/countries/src/components/CountryList.jsx
+++ part2/countries/src/components/CountryList.jsx
@@ -1,29 +1,34 @@
-import CountryDetails from "./CountryDetails"
+import { useState } from "react";
+import CountryDetails from "./CountryDetails";
 
-const CountryList = ({countries}) => {
+function CountryList({ countries }) {
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
     if (countries.length > 10) {
-      return <p>Too many matches, please specify another filter.</p>
+        return <p>Too many matches, specify another filter</p>
     }
-    
-    if (countries.length > 1) {
-      return (
-        <ul>
-          {countries.map(country => (
-            <li key={country.cca3}>{country.name.common}</li>
-          ))}
-        </ul>
-      )
-    }
-    
-    if (countries.length === 1) {
-      const country = countries[0]
-      return (
-          <CountryDetails country={countries[0]} />
+
+    return (
+        <div>
+            <ul>
+                {countries.map(country => (
+                    <li key={country.cca3}>
+                        {country.name.common}{' '}
+                        <button onClick={() =>
+                            selectedCountry?.cca3 === country.cca3
+                                ? setSelectedCountry(null)
+                                : setSelectedCountry(country)}>
+                            {selectedCountry?.cca3 === country.cca3 ? 'Hide' : 'Show'}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            {selectedCountry && (
+                <CountryDetails country={selectedCountry} />
+            )}
+        </div>
     )
-    }
-    
-    return null;
 }
 
 export default CountryList;
-  
